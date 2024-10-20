65d2e90c81c5beae3f469752cb9c0596d7d62a06
AndraWahyuSaputra <andrawahyu004@gmail.com>
2024-10-20 00:12:26 -0700

    Add files via upload

---
+++ latihan4.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+
+void printMessage() {
+    cout << "Hello, world!" << endl;
+
+}
+
+int main() {
+    printMessage(); // di bagian printMessage  perlu tanda kurung () agar print message yang diatas terpanggil
+    return 0;
+
+}
